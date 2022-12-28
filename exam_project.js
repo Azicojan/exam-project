7827567985c5e6ff4e22e9ad1c1fe0a9c2fad578
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-12-28 13:04:39 +0500

    still doing my exam project

--- exam_project.js
+++ exam_project.js
@@ -1,1 +1,41 @@
-console.log("Hello world")
+
+//today 
+let today = document.querySelector('.menu_today');
+today.addEventListener("mouseover",mouseOver);
+today.addEventListener("mouseout",mouseOut);
+
+
+function mouseOver(){
+    today.style.cssText=`
+    
+    border-right: 1px solid grey;
+    border-left: 1px solid grey;
+    border-bottom: 3px solid rgb(45, 166, 170); `;
+};
+
+function mouseOut(){
+    today.style.cssText=`
+    border-style:none;`
+}
+
+
+// a five-day forecast
+
+let fiveDay = document.querySelector('.menu_5day');
+fiveDay.addEventListener("mouseover", fiveDayOver);
+fiveDay.addEventListener("mouseout",fiveOverOut);
+
+function fiveDayOver(){
+    fiveDay.style.cssText=`
+    
+    border-right: 1px solid grey;
+    border-left: 1px solid grey;
+    border-bottom: 3px solid rgb(45, 166, 170); `;
+};
+
+function fiveOverOut(){
+    fiveDay.style.cssText=`
+    border-style:none;`
+}
+console.log(today);
+console.log(fiveDay)
